cad490c1820a5f7badf2179d400a79f70c4b2e8f
emav <emanuele.quinto@gmail.com>
2014-05-08 00:25:11 +0200

    ADD stats to example.js

--- example.js
+++ example.js
@@ -1,3 +1,10 @@
-require('./').watch('lib', {ignored: /[\/\\]\./}).on('all', function(event, path) {
-  console.log(event, path);
-});
+console.log("Hello world");
+//require('./').watch('tmp', {ignored: /[\/\\]\./}).on('all', function(event, path) {
+require('./').watch('tmp', { persistent: true })
+  .on('all', function(event, path) {
+    console.log(event, path);
+  })
+  .on('add', function(path, stats) {
+    console.log('File', path, 'stats', stats);
+  });
+console.log("done");
